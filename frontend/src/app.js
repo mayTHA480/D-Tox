a49c5119ee677e631536d96713b9b589f1a31600
mayTHA480 <maythazinhtun001@gmail.com>
2026-05-20 12:12:23 +0800

    emergency singleplayer fix

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,19 +1,28 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import Login from './components/Login';
+// import SinglePlayer from './components/SinglePlayer';
+// import Multiplayer from './components/Multiplayer';
+// import './index.css';
+
+// // Route to the right component based on which root div exists
+// const loginRoot = document.getElementById('root-login');
+// const singleRoot = document.getElementById('root-singleplayer');
+// const multiRoot = document.getElementById('root-multiplayer');
+
+// if (loginRoot) {
+//   ReactDOM.createRoot(loginRoot).render(<Login />);
+// } else if (singleRoot) {
+//   ReactDOM.createRoot(singleRoot).render(<SinglePlayer />);
+// } else if (multiRoot) {
+//   ReactDOM.createRoot(multiRoot).render(<Multiplayer />);
+// }
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './components/Login';
 import SinglePlayer from './components/SinglePlayer';
-import Multiplayer from './components/Multiplayer';
 import './index.css';
 
-// Route to the right component based on which root div exists
-const loginRoot = document.getElementById('root-login');
-const singleRoot = document.getElementById('root-singleplayer');
-const multiRoot = document.getElementById('root-multiplayer');
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-if (loginRoot) {
-  ReactDOM.createRoot(loginRoot).render(<Login />);
-} else if (singleRoot) {
-  ReactDOM.createRoot(singleRoot).render(<SinglePlayer />);
-} else if (multiRoot) {
-  ReactDOM.createRoot(multiRoot).render(<Multiplayer />);
-}
+root.render(<SinglePlayer />);
